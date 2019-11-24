a9a6d929d2fce583cd701790c555c3822c869f99
Abass Sesay <bassco10@gmail.com>
2019-11-23 17:25:16 -0800

    initial script to parse a hex text file to a binary file

---
+++ hexToBin.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Input and Output files required")
+        return
+
+    source = sys.argv[1]
+    dest = sys.argv[2]
+
+    print("Parsing data")
+    data = []
+    with open(source) as src:
+        for line in src:
+            for _hex in line.split(" "):
+                data.append(int(_hex, 16))
+    print("Done parsing")
+    with open(dest, "wb") as dst:
+        dst.write(bytearray(data))
+
+    print("Complete")
+
+
+if __name__ == "__main__":
+    main()
